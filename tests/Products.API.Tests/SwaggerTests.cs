9a0abdd562b2d2f1b9b1b09a35ebb2254d7db3c2
robertoetcheverryr <yggdrasil.raiker@gmail.com>
2026-09-04 00:34:03 -0300

    Update Swagger Tests to use the Exceptions catalog. Add tests for the full examples

--- tests/Products.API.Tests/SwaggerTests.cs
+++ tests/Products.API.Tests/SwaggerTests.cs
@@ -1,7 +1,8 @@
-﻿using System.Net;
+using System.Net;
 using System.Text.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Products.API.Exceptions;
 
 namespace Products.API.Tests;
 
@@ -55,6 +56,39 @@ public class SwaggerTests : IClassFixture<WebApplicationFactory<Program>>
     }
 
     [Fact]
+    public async Task SwaggerJson_GetById_ShouldIncludeSuccessAndPrd001Examples()
+    {
+        var swagger = await LoadSwaggerAsync();
+        var example200 = GetResponseExample(swagger, "/api/products/{id}", "get", "200");
+        var example404 = GetResponseExample(swagger, "/api/products/{id}", "get", "404");
+
+        example200.Should().Contain("Notebook Dell XPS 15");
+        example404.Should().Contain(ErrorCodes.PRD_001);
+        example404.Should().Contain(ErrorCodes.PRD_001_Message);
+    }
+
+    [Fact]
+    public async Task SwaggerJson_Create_ShouldIncludeRequestAndConflictExamples()
+    {
+        var swagger = await LoadSwaggerAsync();
+        var request = GetRequestExample(swagger, "/api/products", "post");
+        var example409 = GetResponseExample(swagger, "/api/products", "post", "409");
+
+        request.Should().Contain("Notebook Dell XPS 15");
+        example409.Should().Contain(ErrorCodes.PRD_003);
+    }
+
+    [Fact]
+    public async Task SwaggerJson_Delete_ShouldIncludePrd004Example()
+    {
+        var swagger = await LoadSwaggerAsync();
+        var example409 = GetResponseExample(swagger, "/api/products/{id}", "delete", "409");
+
+        example409.Should().Contain(ErrorCodes.PRD_004);
+        example409.Should().Contain(ErrorCodes.PRD_004_Detail);
+    }
+
+    [Fact]
     public async Task SwaggerJson_ShouldDocument500WithErrorResponse()
     {
         var swagger = await LoadSwaggerAsync();
@@ -62,7 +96,7 @@ public class SwaggerTests : IClassFixture<WebApplicationFactory<Program>>
 
         json.Should().Contain("500");
         json.Should().Contain("ErrorResponse");
-        json.Should().ContainEquivalentOf("PRD-005");
+        json.Should().ContainEquivalentOf(ErrorCodes.PRD_005);
     }
 
     private async Task<JsonDocument> LoadSwaggerAsync()
@@ -106,5 +140,39 @@ public class SwaggerTests : IClassFixture<WebApplicationFactory<Program>>
         actualCodes.Should().BeEquivalentTo(
             expectedCodes,
             $"documented HTTP codes for {method.ToUpperInvariant()} {path} must match 4.1");
+    }
+
+    private static string GetResponseExample(
+        JsonDocument swagger,
+        string path,
+        string method,
+        string statusCode)
+    {
+        var operation = GetOperation(swagger, path, method);
+        operation.TryGetProperty("responses", out var responses).Should().BeTrue();
+        responses.TryGetProperty(statusCode, out var response).Should().BeTrue();
+        response.TryGetProperty("content", out var content).Should().BeTrue();
+        content.TryGetProperty("application/json", out var media).Should().BeTrue();
+        media.TryGetProperty("example", out var example).Should().BeTrue(
+            $"{method.ToUpperInvariant()} {path} {statusCode} should have an example");
+        return example.GetRawText();
+    }
+
+    private static string GetRequestExample(JsonDocument swagger, string path, string method)
+    {
+        var operation = GetOperation(swagger, path, method);
+        operation.TryGetProperty("requestBody", out var requestBody).Should().BeTrue();
+        requestBody.TryGetProperty("content", out var content).Should().BeTrue();
+        content.TryGetProperty("application/json", out var media).Should().BeTrue();
+        media.TryGetProperty("example", out var example).Should().BeTrue(
+            $"{method.ToUpperInvariant()} {path} should have a request example");
+        return example.GetRawText();
+    }
+
+    private static JsonElement GetOperation(JsonDocument swagger, string path, string method)
+    {
+        var pathItem = FindPath(swagger, path);
+        pathItem.TryGetProperty(method, out var operation).Should().BeTrue();
+        return operation;
     }
 }
